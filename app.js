d1cde5ba823a763d06288ac593283e309f4c0f54
Furkan Sarı <furkansa@hotmail.com.tr>
2022-06-25 18:13:53 +0300

    Make further changes to the file structure

---
+++ app.js
@@ -0,0 +1,18 @@
+// event listeners
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+function playSound(e) {
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   if (!audio) return; // stop the function from running alltogether
+   audio.currentTime = 0; // rewind to the start
+   audio.play();
+   key.classList.add("playing");
+}
+function removeTransition(e) {
+   if (e.propertyName !== "transform") return; // skip if it's not a transform
+   this.classList.remove("playing");
+}
